933a70122ed03027b3fbcb6ed2c07c35f88463ec
Solnyshkoo <kepetrova@edu.hse.ru>
2021-10-28 14:16:11 +0300

    Set correct input in cell. Make sorted button

--- kepetrovaPW4-agaaaaaaain/NoteViewController.swift
+++ kepetrovaPW4-agaaaaaaain/NoteViewController.swift
@@ -13,31 +13,29 @@ class NoteViewController: UIViewController {
     @IBOutlet weak var countryView: UITextField!
     var outputVC: ViewController!
     var countries: [Country] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-    
-    navigationItem.rightBarButtonItem =
-    UIBarButtonItem(barButtonSystemItem: .done, target: self, action:
-    #selector(didTapSaveNote(button:)))
+        
+        navigationItem.rightBarButtonItem =
+            UIBarButtonItem(barButtonSystemItem: .done, target: self, action:
+                                #selector(didTapSaveNote(button:)))
         
     }
     @objc func didTapSaveNote(button: UIBarButtonItem) {
         let title = titleTextField.text ?? ""
         let description = textView.text ?? ""
-        
+        let countryName = countryView.text ?? ""
         if !title.isEmpty{
             let newNote = Note(context: outputVC.context)
-            //newNote.setValue(title, forKey: "title")
             newNote.title = title
-            //newNote.setValue(description, forKey: "descriptionText")
             newNote.descriptionText = description
             let dateComponents = Calendar.current.dateComponents([.day, .month, .year], from: Date())
             let today: Date? = Calendar.current.date(from: dateComponents)
-            //newNote.setValue(today, forKey: "creationDate")
             newNote.creationDate = today!
             var currentCountry: Country
             for country in countries {
-                if country.fullName == countryView.text{
+                if country.fullName == countryName{
                     currentCountry = country;
                     currentCountry.addToNote(newNote)
                     newNote.origin = currentCountry
@@ -46,7 +44,7 @@ class NoteViewController: UIViewController {
             if newNote.origin == nil {
                 currentCountry = Country(context: outputVC.context)
                 countries.append(currentCountry)
-                currentCountry.fullName = countryView.text
+                currentCountry.fullName = countryName
                 currentCountry.addToNote(newNote)
                 newNote.origin = currentCountry
             }
@@ -54,8 +52,9 @@ class NoteViewController: UIViewController {
             outputVC.saveChanges()
         }
         outputVC.countries = countries
+        
         self.navigationController?.popViewController(animated: true)
     }
-
+    
 }
 
--- kepetrovaPW4-agaaaaaaain/ViewController.swift
+++ kepetrovaPW4-agaaaaaaain/ViewController.swift
@@ -17,16 +17,31 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         let cell =
             collectionView.dequeueReusableCell(withReuseIdentifier:
                                                 "NoteCell", for: indexPath) as! NoteCell
-        let note = notes[indexPath.row]
-        cell.titleLabel.text = note.title
         
-        cell.descriptionLabel.text! += "\n"
+        let note: Note
+        if isFilterOn {
+            note = filteredNotes[indexPath.row]
+        } else {
+            note = notes[indexPath.row]
+        }
+        
+        
+        let nsString = (note.title ?? "") as NSString
+        cell.titleLabel.text = nsString.substring(with: NSRange(location: 0, length: nsString.length > 10 ? 10 : nsString.length))
+        
+        
         let date = note.creationDate
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "YY/MM/dd"
-        cell.descriptionLabel.text =  dateFormatter.string(from: date!) + "\n" + note.descriptionText!
-        cell.countryLabel.text = note.origin?.fullName
         
+        let nsStringdescription = (note.descriptionText ?? "") as NSString
+        let firstLine = nsStringdescription.substring(with: NSRange(location: 0, length: nsStringdescription.length > 10 ? 10 : nsStringdescription.length))
+        let secondLine = nsStringdescription.substring(with: NSRange(location: (nsStringdescription.length > 10 ? 10 : 0), length: (nsStringdescription.length > 20 ? 10 : 0)))
+        
+        cell.descriptionLabel.text =  dateFormatter.string(from: date!) + "\n" + firstLine + "\n" + secondLine
+        
+        let nsStringCountry = (note.origin?.fullName ?? "") as NSString
+        cell.countryLabel.text =  nsStringCountry.substring(with: NSRange(location: 0, length: nsStringCountry.length > 10 ? 10 : nsStringCountry.length))
         return cell
     }
     
@@ -36,6 +51,8 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
             notesCollectionView.reloadData()
         }
     }
+    
+    var filteredNotes: [Note] = []
     
     var countries: [Country] = [] {
         didSet {
@@ -62,8 +79,8 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         navigationItem.rightBarButtonItem =
             UIBarButtonItem(barButtonSystemItem: .add, target: self, action:
                                 #selector(createNote(sender:)))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(named: "filter"), style: .plain , target: self, action: #selector(filter(sender:)))
         
-      
         
     }
     @objc func createNote(sender: UIBarButtonItem) {
@@ -74,8 +91,31 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         }
         vc.outputVC = self
         vc.countries = countries;
+        
         navigationController?.pushViewController(vc, animated: true)
     }
+    private var buttonCount = 0
+    private var isFilterOn = false
+    
+    @objc func filter(sender: UIBarButtonItem) {
+        buttonCount += 1
+        switch buttonCount {
+        case 2:
+            isFilterOn = false
+            saveChanges()
+            
+        case 1:
+            filteredNotes = notes.sorted { (channel1, channel2) -> Bool in
+                let name1 = channel1.origin?.fullName
+                let name2 = channel2.origin?.fullName
+                return (name1?.localizedCaseInsensitiveCompare(name2!) == .orderedAscending)}
+            saveChanges()
+            isFilterOn = true
+        default:
+            buttonCount = 0
+        }
+    }
+    
     
     func saveChanges() {
         if context.hasChanges {
@@ -83,16 +123,26 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
         }
         if let note = try? context.fetch(Note.fetchRequest()) {
             self.notes = (note as? [Note] ?? [])
+            self.filteredNotes = (note as? [Note] ?? []).sorted { (channel1, channel2) -> Bool in
+                let name1 = channel1.origin?.fullName
+                let name2 = channel2.origin?.fullName
+                return (name1?.localizedCaseInsensitiveCompare(name2!) == .orderedAscending)}
         } else {
             self.notes = []
+            self.filteredNotes = []
         }
     }
     
     func loadData() {
         if let notes = try? context.fetch(Note.fetchRequest()) {
             self.notes = (notes as? [Note])!
+            self.filteredNotes = (notes as? [Note])!.sorted { (channel1, channel2) -> Bool in
+                let name1 = channel1.origin?.fullName
+                let name2 = channel2.origin?.fullName
+                return (name1?.localizedCaseInsensitiveCompare(name2!) == .orderedAscending)}
         } else {
             self.notes = []
+            self.filteredNotes = []
         }
     }
     
@@ -105,14 +155,16 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
             let deleteAction = UIAction(title: "Delete", image:
                                             UIImage(systemName: "trash"), attributes:
                                                 UIMenuElement.Attributes.destructive) { value in
+                self.notes[indexPath.row].origin?.removeFromNote(self.notes[indexPath.row])
+                
                 self.context.delete(self.notes[indexPath.row])
                 self.saveChanges()
             }
-            return UIMenu(title: "", image: nil, children: [deleteAction])
+                                            return UIMenu(title: "", image: nil, children: [deleteAction])
             
         }
         
     }
-
+    
 }
 
